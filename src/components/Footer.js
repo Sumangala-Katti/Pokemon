6066c2b030c1f7595348070720ec2990b6db67ac
sumangala <sumakatti009@gmail.com>
2024-06-19 21:44:02 +0530

    Initial Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-green-950 text-center text-white py-4 text-xl">
+      <p>&copy; Pokemon | All Rights Reserved</p>
+      <p>Created by: Sumangala Katti</p>
+    </div>
+  );
+};
+
+export default Footer;
